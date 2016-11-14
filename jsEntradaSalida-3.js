e63eafdcf14bac74ebe391700a3ce4ca333b6e90
FacundoSerrano <facundo.jose.serrano@gmail.com>
2016-11-14 16:44:21 +0200

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{//"CamelCase" si la variable tiene varias palabras, la primera siempre va en minus, y las otras comienzan con mayus.
+//document. + tab
+//.value asigna el valor a un objeto
+	var miVariable=document.getElementById('elNombre').value;
+	alert(miVariable);
+	
+}
+
+
