b0d5c90db3e77d6edf47a8b76cca8d6520ff69f8
FacundoSerrano <facundo.jose.serrano@gmail.com>
2016-11-18 12:10:36 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,44 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-
-
-
-}//FIN DE LA FUNCIÓN
+	var mes;
+	mes=document.getElementById('mes').value;
+	switch mes(0;11);
+	{
+    case 0:
+        mes="Enero";
+        text
+        break;
+    case 1:
+        mes="Febrero";
+        break;
+    case 2:
+        mes="Marzo";
+        break;
+    case 3:
+        mes="Abril";
+        break;
+    case 4:
+        mes="Mayo";
+        break;
+    case 5:
+        mes="Junio";
+        break;
+    case 6:
+        mes="Julio";
+    case 7:
+        mes="Agosto";
+        break;
+    case 8:
+        mes="Septiembre";
+    case 9:
+        mes="Octubre";
+        break;
+    case 10:
+        mes="Noviembre";
+    case 11:
+        mes="Diciembre";
+        break;
+	}
+}
+//FIN DE LA FUNCIÓN
