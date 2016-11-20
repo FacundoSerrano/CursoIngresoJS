df2f31e53077a9774ff474032764837c035db6fe
FacundoSerrano <facundo.jose.serrano@gmail.com>
2016-11-19 23:25:50 -0300

    Delete jsEntradaSalida-1.js

--- jsEntradaSalida-1.js
+++
@@ -1,6 +0,0 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostar()
-{//Mensaje. Comando siempre en minus y sin espacio. Alert para mostrar un mensaje.
-		alert("Esto funciona de maravilla");
-}
-
